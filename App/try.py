be5ecd0f0dee16b77d82cf0cf6481d160d5ff853
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-07-09 12:31:37 +0530

    updated

--- App/try.py
+++ App/try.py
@@ -3,61 +3,81 @@ from collections import defaultdict
 from io import BytesIO
 from PIL import Image
 from kivymd.app import MDApp
-from kivy.animation import Animation
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.card import MDCard
-from kivymd.uix.label import MDLabel
-from kivymd.uix.dialog import MDDialog
+from kivymd.uix.appbar import MDTopAppBar,MDActionTopAppBarButton,MDTopAppBarTitle,MDTopAppBarTrailingButtonContainer,MDTopAppBarLeadingButtonContainer
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.loadingindicator import MDLoadingIndicator
+from kivymd.uix.list import MDList,MDListItem,MDListItemHeadlineText
+from kivymd.uix.textfield import MDTextField,MDTextFieldHelperText,MDTextFieldTrailingIcon,MDTextFieldHintText,MDTextFieldLeadingIcon
+from kivymd.uix.fitimage import FitImage
 from kivymd.uix.scrollview import MDScrollView
+from kivymd.uix.card import MDCard
 from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.button import MDButton,MDButtonText,MDIconButton
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.toolbar import MDTopAppBar
 from kivy.metrics import dp
 from kivy.uix.image import Image as KivyImage
 from kivy.core.image import Image as CoreImage
+from kivy.uix.screenmanager import NoTransition,ScreenManager
 
 cards = defaultdict()
 
-class MDHoverCard(MDCard):
-    def on_enter(self,*args):Animation(elevation=8,duration=0.15).start(self)
-    def on_leave(self,*args):Animation(elevation=2,duration=0.15).start(self)
+class AdminScreen(MDScreen):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        root = MDFloatLayout()
+        self.add_widget(root)
 
-class MainApp(MDApp):
+class HomeScreen(MDScreen):
     def assignCard(self,i):
-        card = MDHoverCard(size_hint=(1, None),height="100dp",elevation=2,on_release=lambda x:print(i))
+        card = MDCard(size_hint=(1,None),height='100dp',elevation=2,on_release=lambda x:print(i))
         card.data = {'Name':''}
         cards[i] = card
-        self.content.add_widget(card)
-        
+        self.layout.add_widget(card)
     def refreshPage(self,updater):
         connection = mysql.connector.connect(host='127.0.0.1',user='root',password='Nikish@2003',database='pentecostmatrimony')
         cursor = connection.cursor(dictionary=True)
         cursor.execute('''select * from userData where role = "user" limit %s offset %s''',(self.limits,self.page*self.limits,))
         result = cursor.fetchall()
         for i in result:
-            i=list(i)
-            i.pop(1);print(i)
+            i.pop('Photo');print(i)
         self.page += 1 if updater else -1
         cursor.close()
         connection.close()
-        
-    def build(self):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
         self.page = 1
         self.limits = 30
         root = MDFloatLayout()
-        toolbar = MDTopAppBar(title="User Form",pos_hint={"top": 1},left_action_items=[["arrow-left", lambda x: print("Home")]],)
-        scroll = MDScrollView(size_hint=(1, 0.9),pos_hint={"x":0,"y":0},)
-        self.content = MDGridLayout(cols=3,adaptive_height=True,spacing="10dp",padding="10dp",)
+        root.add_widget(MDTopAppBar(MDTopAppBarLeadingButtonContainer(MDActionTopAppBarButton(icon='logout',on_release=lambda x:setattr(self.manager,'current','login'))),
+            MDTopAppBarTitle(text='User Profiles',halign='center'),MDTopAppBarTrailingButtonContainer(
+                MDActionTopAppBarButton(icon='account-circle-outline',on_release=lambda x:print('Menu')),
+                MDActionTopAppBarButton(icon='menu',on_release=lambda x:print('Menu'))),
+            type='small',pos_hint={'top':1},))
+        scroll = MDScrollView(pos_hint={'top':0.89},size_hint=(1,0.89))
+        self.layout = MDGridLayout(cols=3,adaptive_height=True,size_hint=(1,None),spacing='10dp',padding='10dp')
         for i in range(self.limits):self.assignCard(i)
-        print(cards)
-        scroll.add_widget(self.content)
-        buttonFrame = MDBoxLayout(orientation="horizontal",size_hint=(1, 0.1),pos_hint={"y":0},spacing="20dp",padding="20dp")
-        buttonFrame.add_widget(MDFlatButton(text='Previous',on_release=lambda x:self.refreshPage(0)))
-        buttonFrame.add_widget(MDFlatButton(text='Next',on_release=lambda x:self.refreshPage(1)))
+        scroll.add_widget(self.layout)
         root.add_widget(scroll)
-        root.add_widget(toolbar)
-        root.add_widget(buttonFrame)
-        return root
+        root.add_widget(MDButton(MDButtonText(text='Next')))
+        self.add_widget(root)
+
+class LoginScreen(MDScreen):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        layout = MDFloatLayout()
+        layout.add_widget(FitImage(source='icons&Images/image1.png',size_hint=(1,1)))
+        loginCard = MDCard(orientation='vertical',md_bg_color=(0.12, 0.12, 0.12, 1),size_hint=(0.4,0.4),pos_hint={'center_x':0.5,'center_y':0.5},elevation=2)
+        loginCard.add_widget(MDTextField(MDTextFieldLeadingIcon('account'),MDTextFieldHelperText(text='Enter Your Email',mode="persistent")))
+        loginCard.add_widget(MDIconButton(icon='home',on_release=lambda x:setattr(self.manager,'current','home')))
+        layout.add_widget(loginCard)
+        self.add_widget(layout)
+        
+class MainApp(MDApp):      
+    def build(self):
+        screenManager = ScreenManager(transition=NoTransition())
+        screenManager.add_widget(LoginScreen(name='login'))
+        screenManager.add_widget(HomeScreen(name='home'))
+        return screenManager
 
 MainApp().run()
