b61dbadcb3881145816b84af19f64e7b5aa03a57
nikishdaniel1-del <nikishdaniel1@gmail.com>
2026-07-09 17:19:33 +0530

    updated

--- App/try.py
+++ App/try.py
@@ -3,8 +3,11 @@ from collections import defaultdict
 from io import BytesIO
 from PIL import Image
 from kivymd.app import MDApp
+from kivymd.icon_definitions import md_icons
+from kivymd.uix.filemanager import MDFileManager
 from kivymd.uix.appbar import MDTopAppBar,MDActionTopAppBarButton,MDTopAppBarTitle,MDTopAppBarTrailingButtonContainer,MDTopAppBarLeadingButtonContainer
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.pickers import MDModalDatePicker,MDDockedDatePicker,MDModalInputDatePicker
 from kivymd.uix.loadingindicator import MDLoadingIndicator
 from kivymd.uix.list import MDList,MDListItem,MDListItemHeadlineText
 from kivymd.uix.textfield import MDTextField,MDTextFieldHelperText,MDTextFieldTrailingIcon,MDTextFieldHintText,MDTextFieldLeadingIcon
@@ -12,7 +15,7 @@ from kivymd.uix.fitimage import FitImage
 from kivymd.uix.scrollview import MDScrollView
 from kivymd.uix.card import MDCard
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.button import MDButton,MDButtonText,MDIconButton
+from kivymd.uix.button import MDButton,MDButtonText,MDIconButton,MDExtendedFabButtonIcon,MDExtendedFabButton,MDExtendedFabButtonText
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
 from kivy.metrics import dp
@@ -21,11 +24,27 @@ from kivy.core.image import Image as CoreImage
 from kivy.uix.screenmanager import NoTransition,ScreenManager
 
 cards = defaultdict()
+fieldList = ['Name','Profession','Date of birth','Gender','Qualification','Height','Income','Background','Marital Status','Languages Known',"Father's Name","Mother's Name",
+             "Parent's Number",'Whatsapp Number','Family Status','Hometown','Current Resident Address','Siblings','Local Faith Home','Centre Faith Home','Expectations']
+print(md_icons.keys())
+
+def userForm():
+    scroll = MDScrollView(size_hint=(1, 0.9),pos_hint={'x':0,'y':0})
+    layout = MDBoxLayout(orientation='vertical',adaptive_height=True,spacing='10dp',padding='10dp')
+    for i in fieldList:layout.add_widget(MDTextField())
+    return scroll
+
+class FormScreen(MDScreen):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        root = MDFloatLayout()
+        self.add_widget(root)
 
 class AdminScreen(MDScreen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         root = MDFloatLayout()
+        root.add_widget(MDButton(MDButtonText(text='Back'),on_release=lambda x:setattr(self.manager,'current','login')))
         self.add_widget(root)
 
 class HomeScreen(MDScreen):
@@ -49,6 +68,7 @@ class HomeScreen(MDScreen):
         self.page = 1
         self.limits = 30
         root = MDFloatLayout()
+        root.add_widget(FitImage(source='icons&Images/userbg.png',size_hint=(1,1)))
         root.add_widget(MDTopAppBar(MDTopAppBarLeadingButtonContainer(MDActionTopAppBarButton(icon='logout',on_release=lambda x:setattr(self.manager,'current','login'))),
             MDTopAppBarTitle(text='User Profiles',halign='center'),MDTopAppBarTrailingButtonContainer(
                 MDActionTopAppBarButton(icon='account-circle-outline',on_release=lambda x:print('Menu')),
@@ -68,16 +88,20 @@ class LoginScreen(MDScreen):
         layout = MDFloatLayout()
         layout.add_widget(FitImage(source='icons&Images/image1.png',size_hint=(1,1)))
         loginCard = MDCard(orientation='vertical',md_bg_color=(0.12, 0.12, 0.12, 1),size_hint=(0.4,0.4),pos_hint={'center_x':0.5,'center_y':0.5},elevation=2)
-        loginCard.add_widget(MDTextField(MDTextFieldLeadingIcon('account'),MDTextFieldHelperText(text='Enter Your Email',mode="persistent")))
+        loginCard.add_widget(MDTextField(MDTextFieldLeadingIcon(icon='email'),MDTextFieldHelperText(text='Enter Your Email'),MDTextFieldHintText(text='Email')))
+        loginCard.add_widget(MDTextField(MDTextFieldLeadingIcon(icon='lock'),MDTextFieldHelperText(text='Enter Your Password'),MDTextFieldHintText(text='Password'),MDTextFieldTrailingIcon(icon='')))
         loginCard.add_widget(MDIconButton(icon='home',on_release=lambda x:setattr(self.manager,'current','home')))
+        loginCard.add_widget(MDExtendedFabButton(MDExtendedFabButtonIcon(icon='account'),MDExtendedFabButtonText(text='Admin'),on_release=lambda x:setattr(self.manager,'current','admin')))
         layout.add_widget(loginCard)
         self.add_widget(layout)
         
-class MainApp(MDApp):      
+class MainApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = 'Dark'
         screenManager = ScreenManager(transition=NoTransition())
         screenManager.add_widget(LoginScreen(name='login'))
         screenManager.add_widget(HomeScreen(name='home'))
+        screenManager.add_widget(AdminScreen(name='admin'))
         return screenManager
 
 MainApp().run()
